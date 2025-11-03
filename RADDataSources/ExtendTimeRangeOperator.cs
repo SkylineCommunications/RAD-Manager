b31bdd2d4665f6509b6e2a9f4d19cd5e8d8ef223
Tobe Deprez <tobe.deprez@skyline.be>
2025-11-03 14:53:14 +0100

    Show at least three hours when showing an anomaly

--- RADDataSources/ExtendTimeRangeOperator.cs
+++ RADDataSources/ExtendTimeRangeOperator.cs
@@ -13,24 +13,33 @@
 			IsRequired = true,
 		};
 
+		private GQIDoubleArgument _minimumHoursArg = new GQIDoubleArgument("Minimum duration in hours")
+		{
+			DefaultValue = 0,
+			IsRequired = false,
+		};
+
 		private double _factor;
+		private double _minimumHours;
 
 		public GQIArgument[] GetInputArguments()
 		{
-			return new GQIArgument[] { _factorArg };
+			return new GQIArgument[] { _factorArg, _minimumHoursArg };
 		}
 
 		public OnArgumentsProcessedOutputArgs OnArgumentsProcessed(OnArgumentsProcessedInputArgs args)
 		{
 			if (!args.TryGetArgumentValue(_factorArg, out _factor))
-			{
 				throw new ArgumentException($"Required argument factor was not provided");
-			}
 
 			if (_factor <= 0)
-			{
 				throw new ArgumentException($"Factor must be greater than 0");
-			}
+
+			if (!args.TryGetArgumentValue(_minimumHoursArg, out _minimumHours))
+				_minimumHours = 0;
+
+			if (_minimumHours < 0)
+				throw new ArgumentException($"Minimum duration can not be negative");
 
 			return default;
 		}
@@ -45,12 +54,16 @@
 			if (existingTimeRange == null)
 				return;
 
-			var duration = existingTimeRange.EndTime - existingTimeRange.StartTime;
+			var startTime = existingTimeRange.StartTime;
+			var minimumEndTime = startTime.AddHours(_minimumHours);
+			var endTime = existingTimeRange.EndTime < minimumEndTime ? minimumEndTime : existingTimeRange.EndTime;
+
+			var duration = endTime - startTime;
 			double multiplier = (_factor - 1) / 2;
 			var newTimeRange = new TimeRangeMetadata()
 			{
-				StartTime = existingTimeRange.StartTime.AddSeconds(-duration.TotalSeconds * multiplier),
-				EndTime = existingTimeRange.EndTime.AddSeconds(duration.TotalSeconds * multiplier),
+				StartTime = startTime.AddSeconds(-duration.TotalSeconds * multiplier),
+				EndTime = endTime.AddSeconds(duration.TotalSeconds * multiplier),
 			};
 
 			row.Metadata = new GenIfRowMetadata(existingMetaData.Where(m => !(m is TimeRangeMetadata)).Concat(new[] { newTimeRange }).ToArray());
