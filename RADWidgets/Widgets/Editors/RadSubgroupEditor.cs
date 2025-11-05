fb01c6a2cdb75c68e5f2a2150f363a5ab0aeda18
Tobe Deprez <tobe.deprez@skyline.be>
2025-11-05 15:04:30 +0100

    Do not repeat labels element, parameter, display key in subgroup editor

--- RADWidgets/Widgets/Editors/RadSubgroupEditor.cs
+++ RADWidgets/Widgets/Editors/RadSubgroupEditor.cs
@@ -31,6 +31,10 @@
 			if (settings != null)
 				_otherSubgroups = _otherSubgroups.Where(s => s.ID != settings.ID).ToList();
 
+			var elementsLabel = new Label("Element");
+			var parametersLabel = new Label("Parameter");
+			var instanceLabel = new Label("Display key");
+
 			_parameterSelectors = new List<Tuple<Label, ParameterInstanceSelector>>(_parameterLabels.Count);
 			for (int i = 0; i < _parameterLabels.Count; i++)
 			{
@@ -62,6 +66,11 @@
 			int row = 0;
 			AddSection(_groupNameSection, row, 0);
 			row += _groupNameSection.RowCount;
+
+			AddWidget(elementsLabel, row, 1);
+			AddWidget(parametersLabel, row, 2);
+			AddWidget(instanceLabel, row, 3);
+			row++;
 
 			foreach (var (label, parameterSelector) in _parameterSelectors)
 			{
