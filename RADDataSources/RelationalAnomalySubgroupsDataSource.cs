668d7fb8804d6e6be4eb0e287533a83a9c15b84c
Veerle Ledoux <veerle.ledoux@skyline.be>
2025-12-24 10:17:12 +0100

    fixed nullable type

--- RADDataSources/RelationalAnomalySubgroupsDataSource.cs
+++ RADDataSources/RelationalAnomalySubgroupsDataSource.cs
@@ -11,7 +11,7 @@ namespace RadDataSources
 
 	public class RadSubgroupRow : RadGroupBaseRow
 	{
-		public RadSubgroupRow(RadHelper radHelper, RadGroupInfo groupInfo, RadSubgroupInfo subgroupInfo, string subgroupName, FitScore? fitScore)
+		public RadSubgroupRow(RadHelper radHelper, RadGroupInfo groupInfo, RadSubgroupInfo subgroupInfo, string subgroupName, FitScore fitScore)
 			: base(name: subgroupName,
 				 dataMinerID: groupInfo.DataMinerID,
 				 parameters: subgroupInfo.Parameters?.Select(p => p?.Key).WhereNotNull().ToList() ?? new List<ParameterKey>(),
@@ -22,10 +22,10 @@ namespace RadDataSources
 			ParentGroup = groupInfo.GroupName;
 			SubgroupID = subgroupInfo.ID;
 			HasError = !subgroupInfo.IsMonitored;
-			if (fitScore.HasValue)
+			if (fitScore != null)
 			{
-				ModelFitScore = fitScore.Value.ModelFit;
-				IsOutlier = fitScore.Value.IsOutlier;
+				ModelFitScore = fitScore.ModelFit;
+				IsOutlier = fitScore.IsOutlier;
 			}
 			else
 			{
@@ -180,7 +180,7 @@ namespace RadDataSources
 				}
 
 				string subgroupDisplayName = string.IsNullOrEmpty(subgroupInfo.Name) ? RadUtils.Utils.GetSubgroupPlaceHolderName(++unnamedSubgroupCount) : subgroupInfo.Name;
-				FitScore? fitScore;
+				FitScore fitScore;
 				if (fitScores != null && fitScores.TryGetValue(subgroupInfo.ID, out FitScore score))
 					fitScore = score;
 				else
